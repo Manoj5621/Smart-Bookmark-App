b65b37cbd32f3e28bc2341b8014a0f05ed7f7a13
manojr-spec <manojr@spaising.com>
2026-02-18 03:07:01 +0530

    Fix login prerender by using dynamic client-only auth page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,6 +4,8 @@ import dynamic from 'next/dynamic';
 
 const LoginForm = dynamic(() => import('./LoginForm'), { ssr: false });
 
+export const dynamic = 'force-dynamic';
+
 export default function LoginPage() {
   return <LoginForm />;
 }
